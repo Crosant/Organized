58c7e6beecd2afa5b514c566da300b55e8372329
Crosant <florian@reichmuth.de>
2014-05-10 11:32:01 +0200

    Started user class

---
+++ classes/user.class.php
@@ -0,0 +1,45 @@
+<?php
+class user
+{
+    private $Name = "";
+    private ${'Class'} = "";
+	protected $LoggedIn = false;
+    
+	public function __construct()
+	{
+		$this->setName("Guest");
+		$this->setClass("");
+		$this->logout();
+	}
+	
+	public function getName()
+	{
+		return $this->name;
+	}
+	public function getClass()
+	{
+		return $this->{'Class'};
+	}
+	public function getLoggedIn()
+	{
+		return $this->LoggedIn;
+	}
+	
+	public function setName($name)
+	{
+		return $this->name = $name;
+	}
+	public function setClass($class)
+	{
+		return $this->{'Class'} = $class;
+	}
+	public function login()
+	{
+		return $this->LoggedIn = true;
+	}
+	public function logout()
+	{
+		return $this->LoggedIn = false;
+	}	
+}
+?>
